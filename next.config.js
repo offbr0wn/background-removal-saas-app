d6b34eea16a7470b1837df0aac2f5f6c19e04606
Shashank <shashank201011@gmail.com>
2025-02-18 01:13:36 +0000

    .

--- next.config.js
+++ next.config.js
@@ -2,10 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
-    clientMaxBodySize: "5gb", // This sets the maximum body size for client requests
-
     serverActions: {
-      bodySizeLimit: "2gb",
+      bodySizeLimit: "5gb",
     },
   },
 };
