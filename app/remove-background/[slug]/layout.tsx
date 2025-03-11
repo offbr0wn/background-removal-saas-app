e9e06049c200a6b0da708943b573cb722436d52e
Shashank <shashank201011@gmail.com>
2025-03-11 13:15:06 +0000

    ..

--- app/remove-background/[slug]/layout.tsx
+++ app/remove-background/[slug]/layout.tsx
@@ -6,6 +6,12 @@ import { Toaster } from "@/components/ui/toaster";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3025338220182456"
+        />
+      </head>
       <Toaster />
       <body>{children}</body>
     </html>
